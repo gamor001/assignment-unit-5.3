c34453422bb3d789aaba9ecb352a093b630feb90
Brie Morris <morris.brie@gmail.com>
2023-07-02 13:07:22 -0500

    "made album collection function"

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,23 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let addCollection = {
+  title: title,
+  artist: artist,
+  yearPublished: yearPublished,
+  }
+  collection.push(addCollection);
+  return addCollection;
+}
+
+console.log(addToCollection('Pleasure', 'Janelle Monae', 2023 ));
+console.log(addToCollection('Dirty Computer', 'Janelle Monae', 2018));
+console.log(addToCollection('Epiphany' , 'T-Pain' , 2007));
+console.log(addToCollection('Rappa Ternt Sanga' , 'T-Pain' , 2005));
+console.log(addToCollection('Ctrl' , 'SZA' , 2017));
+console.log(addToCollection('Cupid Deluxe' , 'Blood Orange' , 2013));
+
+
+
